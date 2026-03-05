ec7ad303f39c3a1271296b288b09c13caab0a203
aathi-ch <>
2026-03-05 09:32:59 +0000

    Update and Delete

---
+++ ddl_et_dml_en_sql/update_and_delete/query.sql
@@ -0,0 +1,10 @@
+update medications
+    set price = 10
+    where price < 10;
+
+
+
+-- Please do not modify the code below!
+SELECT * FROM medications
+ORDER BY id;
+-- Please do not modify the code above!
